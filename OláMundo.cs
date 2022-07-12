dd6148f32a047f9b21452202518bcdb50cf36ea5
Bruno Diego <103545537+BrunoDiego67@users.noreply.github.com>
2022-07-12 13:48:50 -0300

    Add files via upload

---
+++ OláMundo.cs
@@ -0,0 +1,15 @@
+namespace OlaMundo
+{
+0 referências
+class variaveis
+{
+0 referências
+static void Main()
+{
+int x = 4;
+double y = 3.3;
+const double frequencia = 60;
+double area = x* y;
+}
+}
+}
